4c7f14b7de76ca88cfd84d2d6391f54d54dc02d7
Kristian Holme <holme.kristian@gmail.com>
2024-01-26 16:40:29 +0100

    changed some keybindings

--- lua/kristian/remap.lua
+++ lua/kristian/remap.lua
@@ -2,9 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set("n", "<leader>p", [["/_p]])
---vim.keymap.set("i", "<C-c>", "<Esc>")
---vim.keymap.set("i", "<C-c>", "<Esc>")
+--vim.keymap.set("n", "<leader>p", [["/_p]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
